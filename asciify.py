6c626d2e3bae2ecc863d1fcf2b7aa8a99cee4035
Back777space <ivan.molenaers@hotmail.com>
2022-06-16 22:30:24 +0200

    luminance alogrithm and colours

--- asciify.py
+++ asciify.py
@@ -1,8 +1,6 @@
 from PIL import Image, ImageEnhance, ImageOps
-from termcolor import colored
 
 brightness_chars = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1[]?-_+~<>i!lI;:,"^`."""
-mapping_factor = 255/(len(brightness_chars) - 1)
 
 path = input("Enter the path to your image: ")
 img = Image.open(path)
@@ -10,30 +8,41 @@ img = Image.open(path)
 img = ImageOps.exif_transpose(img)
 
 def cleanup():
-    resize(50)
-    add_contrast(1.9)
+    resize(100)
+    add_contrast(1.6)
     #img.save("edited.png")
 
 def resize(size):
     global img
     aspect_ratio = img.width/img.height
-    new_res = (int(size*aspect_ratio), size)
+    # keep w / h ratio and compensate for vertical spacing in terminal 
+    new_res = (int(size*aspect_ratio), int(size*0.5))
     img = img.resize(new_res)
     
-def add_contrast(scale):
+def add_contrast(factor):
     global img
-    img = ImageEnhance.Contrast(img).enhance(scale)
+    img = ImageEnhance.Contrast(img).enhance(factor)
 
 def to_coloured(col):
     return f"\x1b[38;2;{col[0]};{col[1]};{col[2]}m"
+
+def rgb_to_lin(channel):
+    linear_px = channel/255
+    if linear_px <= 0.04045:
+        return linear_px / 12.92
+    else:
+        return  ((linear_px + 0.055)/1.055)**2.4
+
+def luminance(px):
+    return 0.2126 * rgb_to_lin(px[0]) + 0.7152 * rgb_to_lin(px[1]) + 0.0722 * rgb_to_lin(px[2])
 
 def print_to_console():
     for i in range(img.height):
         for j in range(img.width):
             pixel = img.getpixel((j,i))
-            avg = sum(pixel) / len(pixel)
-            if (avg != 255 and avg > 5):
-                print(brightness_chars[int(avg/mapping_factor)], end="")
+            lum = luminance(pixel)
+            if (lum > 0.02 and lum < 0.97):
+                print(to_coloured(pixel) + brightness_chars[int(lum*len(brightness_chars))], end="")
             else:
                 print(" ", end="")
         print()
