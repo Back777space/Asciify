2494c02d228746940188ce4cc2890dec753ffac7
Back777space <ivan.molenaers@hotmail.com>
2022-06-16 21:48:22 +0200

    better resizing

--- asciify.py
+++ asciify.py
@@ -1,42 +1,42 @@
 from PIL import Image, ImageEnhance, ImageOps
+from termcolor import colored
 
 brightness_chars = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1[]?-_+~<>i!lI;:,"^`."""
 mapping_factor = 255/(len(brightness_chars) - 1)
 
 path = input("Enter the path to your image: ")
 img = Image.open(path)
+# make sure picture is not rotated
 img = ImageOps.exif_transpose(img)
 
-def clean_pic(contrast):
-    resize()
-    add_contrast(contrast)
-    img.save("new.png")
+def cleanup():
+    resize(50)
+    add_contrast(1.9)
+    #img.save("edited.png")
 
-def resize():
-    global h
-    global w
+def resize(size):
     global img
-    h, w = img.height, img.width
-    while h >= 70 and w >= 70:
-        new_size = (int(w/2), int(h/2))
-        img = img.resize(new_size)
-        w, h = new_size
+    aspect_ratio = img.width/img.height
+    new_res = (int(size*aspect_ratio), size)
+    img = img.resize(new_res)
     
 def add_contrast(scale):
     global img
     img = ImageEnhance.Contrast(img).enhance(scale)
 
+def to_coloured(col):
+    return f"\x1b[38;2;{col[0]};{col[1]};{col[2]}m"
+
 def print_to_console():
-    for i in range(h):
-        for j in range(w):
+    for i in range(img.height):
+        for j in range(img.width):
             pixel = img.getpixel((j,i))
-            
             avg = sum(pixel) / len(pixel)
-            if (avg > 5 and avg < 250):
+            if (avg != 255 and avg > 5):
                 print(brightness_chars[int(avg/mapping_factor)], end="")
             else:
                 print(" ", end="")
         print()
 
-clean_pic(1.25)
+cleanup()
 print_to_console()
